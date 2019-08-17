90ee938c2e4cab08a81f01ae5c7b0d34f6f4be05
timbaev <hometim55@gmail.com>
2019-08-17 16:49:31 +0300

    Update Formula

--- Formula/Localizedgenstrings.rb
+++ Formula/Localizedgenstrings.rb
@@ -2,7 +2,7 @@ class Localizedgenstrings < Formula
   desc "Generate localized strings from extension and storyboards"
   homepage "https://github.com/timbaev/LocalizedGenStrings"
   url "https://github.com/timbaev/LocalizedGenStrings.git",
-      :tag => "0.0.4", :revision => "a1bdeb573c5bf11b292605b7baf3266908949bca"
+      :tag => "0.0.5", :revision => "42016d7dd21d0aca10e56b61a924a0f9788427bd"
   head "https://github.com/timbaev/LocalizedGenStrings.git"
 
   depends_on :xcode => ["10.3", :build]
